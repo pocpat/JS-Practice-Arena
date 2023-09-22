86cbd405f051faa38a254c7497c5b48333ef011a
Elena Kroupkin <pocoat@gmail.com>
2023-09-22 22:04:50 +1200

    jest config for modules path

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '#(.*)': '<rootDir>/node_modules/$1',
+  }
 };
 
 
