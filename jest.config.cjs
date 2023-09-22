c4dcdc2ed2437aeb70d157c31fa981b3b1a6a6b0
Elena Kroupkin <pocoat@gmail.com>
2023-09-22 22:09:12 +1200

    revert jest invokation, mark jest bin as executable

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,10 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '#(.*)': '<rootDir>/node_modules/$1',
-  }
 };
 
 
